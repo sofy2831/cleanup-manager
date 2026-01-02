124c44ace65b9d70b32dee2edd53379309d38437
sofy2831 <s.dumas974@orange.fr>
2026-01-02 17:41:21 +0100

    Refactor Firebase initialization code

--- firebase-init.js
+++ firebase-init.js
@@ -1,10 +1,7 @@
-// firebase-init.js (CDN – sans build)
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// 🔐 Configuration Firebase (la tienne)
 const firebaseConfig = {
   apiKey: "AIzaSyA2qR9FBOobUaCI-Zxrv__pbkIx1IY1QIo",
   authDomain: "cleanup-manager-d9301.firebaseapp.com",
@@ -14,7 +11,8 @@ const firebaseConfig = {
   appId: "1:616451512758:web:9252d85102030092664917"
 };
 
-// 🚀 Initialisation
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
