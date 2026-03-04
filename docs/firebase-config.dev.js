dc04c558908a664551ed5a41aad555f9417eaee0
sofy2831 <s.dumas974@orange.fr>
2026-03-04 11:25:58 +0100

    Create firebase-config.dev.js

---
+++ docs/firebase-config.dev.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "...DEV...",
+  authDomain: "...DEV...",
+  projectId: "...DEV...",
+  storageBucket: "...DEV...",
+  messagingSenderId: "...DEV...",
+  appId: "...DEV...",
+};
