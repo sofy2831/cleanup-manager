d8ea743ad2616a1fdf0e1af399536d0ac590d623
sofy2831 <s.dumas974@orange.fr>
2026-03-04 13:41:19 +0100

    Update firebase-config.prod.js

--- docs/firebase-config.prod.js
+++ docs/firebase-config.prod.js
@@ -1,8 +1,9 @@
 export default {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
+  apiKey: "AIzaSyA2qR9FBOobUaCI-Zxrv__pbkIx1IY1QIo",
+  authDomain: "cleanup-manager-d9301.firebaseapp.com",
+  projectId: "cleanup-manager-d9301",
+  storageBucket: "cleanup-manager-d9301.firebasestorage.app",
+  messagingSenderId: "616451512758",
+  appId: "1:616451512758:web:9252d85102030092664917",
+  measurementId: "G-FYQ1DP9HW1"
 };
